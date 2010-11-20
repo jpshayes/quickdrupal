ae766547b7ff396ab249c68e5c59d8446920c6c6
John Paul Hayes <jpshayes@gmail.com>
2010-11-19 21:53:53 -0800

    testing branch config

--- drupalpress.v.1.0/sites/all/themes/polpo/tpl/block.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block">
-
-<?php if (!empty($block->subject)): ?>
-  <h4><?php print $block->subject ?></h4>
-<?php endif;?>
-
-<?php print $block->content ?>
-</div>
